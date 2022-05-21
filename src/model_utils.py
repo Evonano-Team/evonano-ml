b7216d0d9c2044254a3df7a08f1b6f84843b7984
Raisul Kibria <raisul.inc@gmail.com>
2022-05-21 21:09:48 +0300

    added docstrings

--- src/model_utils.py
+++ src/model_utils.py
@@ -3,6 +3,28 @@ from ensemble_model_class import ensemble_model
 from parameter_specification import num_feature
 
 def load_model(model_name, num_feature, directory):
+    """
+    Loads the sepcified model and returns the object representing that model.
+    
+    This function is a utility function that loads any of the models used in
+    this project. The function reads the model from the specified directory 
+    through methods applicable to the type of model.
+    
+    Parameters
+    ----------
+    model_name : str
+        ``transfomer`` or``ensemble_model``, for simulation part or
+        ``sasa_model`` for SASA calculation
+    num_feature : int
+        Number of features in the training data
+    directory : str
+        absolute or relative directory for saving the models
+      
+    Returns
+    -------
+    object
+        The object of the corresponding model's class
+    """
     if model_name == 'transformer' or model_name == 'sasa_model':
         model = keras.models.load_model(directory)
         return model
