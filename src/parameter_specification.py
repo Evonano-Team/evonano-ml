ed94f6d0849aeafbb791236827495bf44224eecb
Raisul Kibria <raisul.inc@gmail.com>
2022-05-21 21:18:05 +0300

    added docstrings

--- src/parameter_specification.py
+++ src/parameter_specification.py
@@ -2,6 +2,23 @@ import os
 import re
 
 def check_mbtr_sasa_pairing(mbtr_dir, sasa_dir, train = True):
+  """
+  Checks for inconsistency in the MBTR and SASA directories
+   
+  This function assesses whether the data files of MBTR and SASA used
+  are in the same order (by comparing their names), i.e.- each 
+  configuration comes in the same order in both cases for tarining and
+  testing respectively.
+    
+  Parameters
+  ----------
+  mbtr_dir : str
+      Absolute or realtive directory of csv files of MBTR vectors
+  sasa_dir : str
+      Absolute or realtive directory of csv files of SASA values
+  train : bool
+      The truth value of whether the passed directories are for training
+  """
   for mbtr_file, sasa_file in list(zip(sorted(os.listdir(mbtr_dir)), sorted(os.listdir(sasa_dir)))):
       
       if re.sub("_sasa", "", sasa_file) != re.sub("mbtr_data_ngrid2_", "", mbtr_file) and re.sub("_sasa", "", sasa_file[2:]) != re.sub("t_mbtr_data_ngrid2_", "", mbtr_file):
