91d091af897dc5721a192ae6abf099bc65bd81dc
Raisul Kibria <raisul.inc@gmail.com>
2022-05-21 21:22:26 +0300

    added docstrings

--- src/sasa_collector.py
+++ src/sasa_collector.py
@@ -1,10 +1,15 @@
 import os
 import shutil
 
+"""
+This is a utility script to copy the csv files of sasa for newer designs
+as they are produced to the base directory containing previous files.
 
-directory = '/media/evonano04/PDB_SYNTHETIC/'
+A prefix is used to differentiate between data gathered for varying timesteps.
+"""
+directory = './PDB_SYNTHETIC/'
 
 for sfile in os.listdir(directory):
     sasa_dir = directory + sfile + '/' + 'SASA.csv'
-    new_sasa_dir = '/home/cloud-user/evonano-backup/evonano-project/new_sasa/' + 't_' + sfile + '_sasa.csv'
+    new_sasa_dir = './new_sasa/' + 't_' + sfile + '_sasa.csv'
     shutil.copy(sasa_dir, new_sasa_dir)
