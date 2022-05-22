ecf38d348321e9d319d535fe77e4fbb49a7741a1
Raisul Kibria <raisul.inc@gmail.com>
2022-05-22 04:10:23 +0300

    added docstrings

--- src/training_data_generator.py
+++ src/training_data_generator.py
@@ -3,6 +3,26 @@ import pandas as pd
 import os
 
 def mbtr_ds_generator(directory):
+  """
+  Creates a dataset of the MBTR vectors.
+  
+  This function reads all CSV data files containing MBTR vectors
+  generated from data of different design configurations. All the
+  vectors are gathered in a dataset along with the duration of the
+  simulations for the designs.
+  
+  Parameters
+  ----------
+  directory : str
+      absolute or relative directory of the MBTR CSV datasets
+      
+  Returns
+  -------
+  x : array of floats; shape (number of designs * duration, num_feature)
+      The vector array of all the MBTRs in a sequence stacked row-wise
+  timestep_size : list of int
+      duration of each design for the MBTR files in the directory
+  """
   ptr = 0
   timestep_size = []
   if directory[-1] != '/':
@@ -22,6 +42,23 @@ def mbtr_ds_generator(directory):
   return x, timestep_size
 
 def sasa_ds_generator(directory):
+  """
+  Creates a dataset of SASA values.
+  
+  This function reads all CSV data files containing the SASA values
+  corresponding to different design configurations at different states.
+  All the values are gathered in a dataset.
+  
+  Parameters
+  ----------
+  directory : str
+      absolute or relative directory of the SASA CSV datasets
+      
+  Returns
+  -------
+  array of floats; shape (number of designs * duration,)
+      The array of all the SASA values in a sequence stacked row-wise
+  """
   ptr = 0
   if directory[-1] != '/':
     directory = directory + '/'
@@ -37,6 +74,37 @@ def sasa_ds_generator(directory):
   return x
 
 def mbtr_mbtr_ds_generator(directory, window_size, sasa_dir = None, shuffle = False):
+  """
+  Creates a windowed dataset of the MBTR vectors, and SASA values if specified.
+  
+  This function reads all CSV data files containing MBTR vectors
+  generated from data of different design configurations. Then,
+  generates a sliding window based dataset pair of MBTRs. If specified
+  the SASA values are also recorded.
+  
+  Parameters
+  ----------
+  directory : str
+      absolute or relative directory of the MBTR CSV datasets
+  window_size: int
+      The window size to create the data pairs
+  sasa_dir : str or None; default: None
+      absolute or relative directory of the SASA CSV datasets,
+      if ``None`` SASA values are not considered
+  shuffle : bool
+      Used to decide whether or not to shuffle the data order
+      
+  Returns
+  -------
+  dataset_x : array of floats; shape (number of samples, window_size, num_feature)
+      The vector array of all the MBTRs stacked in sliding window method
+  dataset_y : array of floats; shape (number of samples, num_feature)
+      The array of MBTR vectors of the next timesteps of the window
+  y_sasa : arrary of floats;  shape (number of samples, 1)
+      The array of all the SASA values for the next timesteps of the window
+  sasa_zero : array of float; shape (number of designs, 1)
+      The array of initial SASA values (1st timestep) for each design configuration
+  """
   dataset_x = []
   dataset_y = []
   y_sasa = []
